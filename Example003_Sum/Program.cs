6e77230b4e97ffe9bb01cab4b8ec5f63efcdb7ce
Tatjana <taninjawi@mail.ru>
2022-08-07 12:53:38 +0700

    Создали 4 примера

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,9 @@
-﻿int numberA = 15;
-int numberB = 66;
+﻿int numberA = new Random().Next(1,15);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,15);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
+
 
     
