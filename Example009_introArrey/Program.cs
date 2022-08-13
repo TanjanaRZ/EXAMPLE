244868213c42eac3ce7e0c9696be37a0df78c584
Tatjana <taninjawi@mail.ru>
2022-08-13 13:35:43 +0700

    Добавили Example009_introArray

---
+++ Example009_introArrey/Program.cs
@@ -0,0 +1,17 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int[] array = { 51, 22, 37, 14, 58, 65, 17, 48, 94 };
+
+//array[0] = 12;
+//Console.WriteLine(arrey[0]);
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
